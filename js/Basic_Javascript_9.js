2211d1184223c1042fa800f87e5f17f842a5f4ba
levi <levicaseblodgett@gmail.com>
2019-10-09 15:46:15 -0700

    Basic Javascript 9
    -added "Basic_Javascript_9.js"
    -added "Basic_HTML_9.html"

---
+++ js/Basic_Javascript_9.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+        }
+    }
+
+    tick();
+}
